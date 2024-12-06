05cf329821abec3c12c087b4de3eb399a37a9215
DESKTOP-52351T8\Admin <yojhf@naver.com>
2024-12-06 17:12:09 +0900

    no message

--- Assets/Youngjun/Scripts/Etc/DayOfTime.cs
+++ Assets/Youngjun/Scripts/Etc/DayOfTime.cs
@@ -1,6 +1,10 @@
+using InnerSight_Seti;
+using MyFPS;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
+// 9시 출근 ~ 23시 퇴근 (대략 4분 이상)
 public class DayOfTime : MonoBehaviour
 {
     public Light sun; // Directional Light
@@ -18,19 +22,25 @@ public class DayOfTime : MonoBehaviour
     public int startHour = 9;
     public int startMinute = 0;
 
+    // ResetTime
+    public int resetTime = 11;
+    private float timeScale = 0;
+
+
     public System.DateTime VirtualDateTime => virtualDateTime;
 
     void Start()
     {
         // 가상 시간을 초기화
         virtualDateTime = new System.DateTime(startYear, startMonth, startDay, startHour, startMinute, 0);
+       
     }
 
     void Update()
     {
         // 경과 시간 계산
         _timeElapsed = Time.deltaTime;
-        _timeAngle = Time.deltaTime;
+        _timeAngle = Time.time;
 
         //_timeAngle += Time.deltaTime;
         float dayProgress = (_timeElapsed / dayDuration) % 1; // 하루 진행 비율 (0~1)
@@ -38,13 +48,17 @@ public class DayOfTime : MonoBehaviour
 
         // 태양의 각도 업데이트
         float sunAngle = dayAngle * 360f; // 하루 동안 360도 회전
-        sun.transform.rotation = Quaternion.Euler(sunAngle - 90f, 170f, 0f); // 태양 회전
+
+        sun.transform.rotation = Quaternion.Euler(sunAngle - 10f, 170f, 0f); // 태양 회전
 
         // 가상 시간 업데이트
         UpdateVirtualDateTime(dayProgress);
 
         // 시간 텍스트 업데이트
         UpdateTimeText();
+
+        // 하루 사이클 끝 => 정산타임
+        ResetDate();
     }
 
     void UpdateVirtualDateTime(float dayProgress)
@@ -53,7 +67,9 @@ public class DayOfTime : MonoBehaviour
         float minutesPassed = dayProgress * 1440f; // 하루에서 지난 시간(분)
 
         virtualDateTime = virtualDateTime.AddMinutes(minutesPassed);
+
         _timeElapsed %= dayDuration; // 하루가 지나면 초기화
+        _timeAngle %= dayDuration;
     }
 
     void UpdateTimeText()
@@ -63,5 +79,43 @@ public class DayOfTime : MonoBehaviour
             // TextMeshPro에 표시할 텍스트 설정
             timeText.text = virtualDateTime.ToString("yyyy년 MM월 dd일 HH시:mm분");
         }
+    }
+
+    // 하루 정산 기능
+    void ResetDate()
+    {
+        if (virtualDateTime.Hour == resetTime)
+        {
+            StartCoroutine(StartReset());
+        }
+    }
+
+    IEnumerator StartReset()
+    {
+        // 리셋 순서
+        // => 시간 멈춤 -> 정산 UI 켬 -> 정산 UI 끔 -> fadeout -> fadein -> 시간 정상화 -> 가상시간 리셋 -> 플레이 
+        Pause();
+
+
+
+        SceneFade.instance.FadeOut(null);
+
+
+        yield return null;  
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = timeScale;
+    }
+
+    public void ResetPause()
+    {
+        //isPause = false;
+        //pauseUI.gameObject.SetActive(false);
+        // 일시정시 원복
+        Time.timeScale = 1.0f;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //player.GetComponent<FirstPersonController>().enabled = true;
     }
 }
---
+++ Assets/Youngjun/Scripts/Etc/SceneFade.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+namespace MyFPS
+{
+    public class SceneFade : MonoBehaviour
+    {
+        public static SceneFade instance;   
+
+        [SerializeField] private Image fadeImage;
+        public AnimationCurve fadeCurve;
+
+        public bool startFadeIn = true;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        void Start()
+        {
+            fadeImage.color = new Color(0f, 0f, 0f, 255f);
+
+            if (fadeImage.gameObject.activeSelf == false)
+            {
+                fadeImage.gameObject.SetActive(true);
+
+                if (startFadeIn == true)
+                {
+                    FadeIn(null);
+                }
+
+            }
+
+        }
+
+        public void FadeIn(string name, float delay = 0f)
+        {
+            StartCoroutine(FadeIn_Co(name, delay));
+        }
+
+        public void FadeOut(string name, float delay = 0f)
+        {
+            StartCoroutine(FadeOut_Co(name, delay));
+        }
+
+        //public void FadeOut(int name, float delay = 0f)
+        //{
+        //    StartCoroutine(FadeOut_Co(name, delay));
+        //}
+
+
+        IEnumerator FadeIn_Co(string name, float delay)
+        {
+            float time = 1f;
+            float ctime = 0f;
+
+            if(delay > 0f)
+            {
+                yield return new WaitForSeconds(delay);
+            }
+
+     
+
+            while (ctime < time)
+            {
+                float a = fadeCurve.Evaluate(time);
+
+                fadeImage.color = new Color(0f, 0f, 0f, a);
+                time -= Time.deltaTime;
+                yield return null;
+            }
+
+            if (name != null)
+            {
+                SceneManager.LoadScene(name);
+            }
+
+        }
+
+        IEnumerator FadeOut_Co(string name, float delay)
+        {
+            float time = 1f;
+            float ctime = 0f;
+
+            if (delay > 0f)
+            {
+                yield return new WaitForSeconds(delay);
+            }
+
+            while (ctime < time)
+            {
+                float a = fadeCurve.Evaluate(ctime);
+
+                fadeImage.color = new Color(0f, 0f, 0f, a);
+                ctime += Time.deltaTime;
+                yield return null;
+            }
+
+            if (name != null)
+            {
+                SceneManager.LoadScene(name);
+            }
+
+        }
+
+        //IEnumerator FadeOut_Co(int name, float delay)
+        //{
+        //    float time = 1f;
+        //    float ctime = 0f;
+
+        //    if (delay > 0f)
+        //    {
+        //        yield return new WaitForSeconds(delay);
+        //    }
+
+        //    while (ctime < time)
+        //    {
+        //        float a = fadeCurve.Evaluate(ctime);
+
+        //        fadeImage.color = new Color(0f, 0f, 0f, a);
+        //        ctime += Time.deltaTime;
+        //        yield return null;
+        //    }
+
+        //    if (name != null)
+        //    {
+        //        SceneManager.LoadScene(name);
+        //    }
+
+        //}
+
+
+    }
+}
