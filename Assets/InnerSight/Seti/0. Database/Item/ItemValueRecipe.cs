a315eaeb509958c7507c8f3200911e2fb91294fc
xianium92 <xianium92@gmail.com>
2024-12-12 18:04:05 +0900

    1. 랜덤 가격표 적용 완료 2. 상인 NPC 및 상점 UI 기초 구현 완료

--- Assets/InnerSight/Seti/0. Database/Item/ItemValueRecipe.cs
+++ Assets/InnerSight/Seti/0. Database/Item/ItemValueRecipe.cs
@@ -5,7 +5,7 @@ namespace InnerSight_Seti
     /// <summary>
     /// 도감용 아이템 키쌍 변동 변수
     /// </summary>
-    public class ItemValueRecipe : MonoBehaviour
+    public class ItemValueRecipe
     {
         public int codexIndex;
         public bool codexDefine = false;
