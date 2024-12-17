c6d5dea5f444d5a34c6cb44394b37b0214395563
DESKTOP-52351T8\Admin <kts6376@naver.com>
2024-12-17 15:59:58 +0900

    no message

--- Assets/KYS/Scripts/AudioManager.cs
+++ Assets/KYS/Scripts/AudioManager.cs
@@ -1,79 +1,79 @@
-using InnerSight_Seti;
-using UnityEngine;
-using UnityEngine.Audio;
+//using InnerSight_Seti;
+//using UnityEngine;
+//using UnityEngine.Audio;
 
-namespace InnerSight_Kys
-{
-    public class AudioManager : Singleton<AudioManager>
-    {
-        [SerializeField] private AudioMixer audioMixer;
-        [SerializeField] private AudioMixerGroup audioMixerGroupBgm;
-        [SerializeField] private AudioMixerGroup audioMixerGroupSfx;
-        public Sounds[] sounds;
+//namespace InnerSight_Kys
+//{
+//    public class AudioManager : Singleton<AudioManager>
+//    {
+//        [SerializeField] private AudioMixer audioMixer;
+//        [SerializeField] private AudioMixerGroup audioMixerGroupBgm;
+//        [SerializeField] private AudioMixerGroup audioMixerGroupSfx;
+//        //public Sounds[] sounds;
 
-        private string bgmSound = ""; // Currently playing BGM
-        public string BgmSound => bgmSound;
+//        private string bgmSound = ""; // Currently playing BGM
+//        public string BgmSound => bgmSound;
 
-        protected override void Awake()
-        {
-            base.Awake();
+//        protected override void Awake()
+//        {
+//            base.Awake();
 
-            if (audioMixer == null)
-            {
-                return;
-            }
+//            if (audioMixer == null)
+//            {
+//                return;
+//            }
 
-            foreach (var sound in sounds)
-            {
-                // Initialize the AudioSource for each sound
-                var outputGroup = sound.loop ? audioMixerGroupBgm : audioMixerGroupSfx;
-                sound.Initialize(this.gameObject, outputGroup);
-            }
-        }
+//            foreach (var sound in sounds)
+//            {
+//                // Initialize the AudioSource for each sound
+//                var outputGroup = sound.loop ? audioMixerGroupBgm : audioMixerGroupSfx;
+//                sound.Initialize(this.gameObject, outputGroup);
+//            }
+//        }
 
-        public void Play(string name)
-        {
-            Sounds sound = System.Array.Find(sounds, s => s.name == name);
-            if (sound == null)
-            {
-                return;
-            }
-            sound.source.Play();
-        }
+//        public void Play(string name)
+//        {
+//            Sounds sound = System.Array.Find(sounds, s => s.name == name);
+//            if (sound == null)
+//            {
+//                return;
+//            }
+//            sound.source.Play();
+//        }
 
-        public void Stop(string name)
-        {
-            Sounds sound = System.Array.Find(sounds, s => s.name == name);
-            if (sound == null)
-            {
-                return;
-            }
-            sound.source.Stop();
-            if (name == bgmSound)
-            {
-                bgmSound = "";
-            }
-        }
+//        public void Stop(string name)
+//        {
+//            Sounds sound = System.Array.Find(sounds, s => s.name == name);
+//            if (sound == null)
+//            {
+//                return;
+//            }
+//            sound.source.Stop();
+//            if (name == bgmSound)
+//            {
+//                bgmSound = "";
+//            }
+//        }
 
-        public void PlayBgm(string name)
-        {
-            if (bgmSound == name) return;
+//        public void PlayBgm(string name)
+//        {
+//            if (bgmSound == name) return;
 
-            StopBgm();
+//            StopBgm();
 
-            Sounds sound = System.Array.Find(sounds, s => s.name == name);
-            if (sound == null)
-            {
-                return;
-            }
+//            Sounds sound = System.Array.Find(sounds, s => s.name == name);
+//            if (sound == null)
+//            {
+//                return;
+//            }
 
-            bgmSound = name;
-            sound.source.Play();
-        }
+//            bgmSound = name;
+//            sound.source.Play();
+//        }
 
-        public void StopBgm()
-        {
-            Stop(bgmSound);
-        }
-    }
-}
+//        public void StopBgm()
+//        {
+//            Stop(bgmSound);
+//        }
+//    }
+//}
