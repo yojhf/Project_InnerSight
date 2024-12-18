d4f75a931789d4f3e4ac790c7314f495f7f53472
xianium92 <xianium92@gmail.com>
2024-12-18 16:35:30 +0900

    11

--- Assets/InnerSight/Seti/2. Scripts/2. Objects/2. NPC/NPC_Customer.cs
+++ Assets/InnerSight/Seti/2. Scripts/2. Objects/2. NPC/NPC_Customer.cs
@@ -240,7 +240,7 @@ namespace InnerSight_Seti
             }
             catch
             {
-                AIBehaviour(NPC_Behaviour.OUTSIDE);
+                AIBehaviour(NPC_Behaviour.EXITING);
             }
         }
 
